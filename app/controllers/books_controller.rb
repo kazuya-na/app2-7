9b4d7e617c3926ade5e8be97335a8f9d1a5d314d
EC2 Default User <ec2-user@ip-172-31-46-209.ap-northeast-1.compute.internal>
2023-04-21 01:15:37 +0000

    [Add]RSpec前

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,7 +3,7 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     if @book.save
-      flash[:notice] = "Book was successfully created."
+      flash[:notice] = "You have created book successfully."
       redirect_to book_path(@book.id)
     else
       @books = Book.all
@@ -12,15 +12,30 @@ class BooksController < ApplicationController
   end
 
   def index
+    @book = Book.new
     @books = Book.all
   end
 
   def show
-    @book = Book.find(params[:id])
+    @book = Book.new
+    @books = Book.find(params[:id])
+    @user = User.find(@books.user.id)
   end
 
   def edit
     @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    redirect_to book_path(@book.id)
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
   end
 
   private
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,21 +5,24 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
-    @books = Book.all
     @book = Book.new
-  end
-
-  def update
+    @user = User.find(params[:id])
+    @books = Book.find(@user.books.ids)
   end
 
   def edit
     @user = User.find(params[:id])
   end
 
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user.id)
+  end
+
   private
 
   def user_params
-    params.require(:user).permit(:name, :introduction, :email, :encrypted_password, :reset_password_token)
+    params.require(:user).permit(:name, :introduction, :profile_image)
   end
 end
